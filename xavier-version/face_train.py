de621925ed45a29eda6a8662881336c3a5511122
step305 <31113900+step305@users.noreply.github.com>
2019-11-19 14:00:43 +0300

    Update face_train.py

--- xavier-version/face_train.py
+++ xavier-version/face_train.py
@@ -40,10 +40,8 @@ def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree
 
         # Loop through each training image for the current person
         for img_path in image_files_in_folder(os.path.join(train_dir, class_dir)):
-            print(img_path)
             image = face_recognition.load_image_file(img_path)
-            face_bounding_boxes = face_recognition.face_locations(image, model='cnn')
-            print(face_bounding_boxes)
+            face_bounding_boxes = face_recognition.face_locations(image)
 
             if len(face_bounding_boxes) != 1:
                 # If there are no people (or too many people) in a training image, skip the image.
